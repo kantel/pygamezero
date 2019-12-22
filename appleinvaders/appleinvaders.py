5ae16c42d8957a4084289f5fc3bc12561e5f2bc3
Jörg Kantel <joerg@kantel.de>
2019-12-22 15:16:48 +0100

    Apple Invaders finished

--- appleinvaders/appleinvaders.py
+++ appleinvaders/appleinvaders.py
@@ -15,11 +15,32 @@ game_over = False
 gripe = Actor("gripe")
 gripe.pos = midbottom=(CENTER, HEIGHT - 48)
 gripe.speed = 5
+gripe_frame = 0
+
+def gripe_walk_left():
+    global gripe_frame
+    if gripe_frame < 7:
+        gripe_frame += 1
+    else:
+        gripe_frame = 0
+    gripe.image = "gl" + str(int(gripe_frame))
+
+
+def gripe_walk_right():
+    global gripe_frame
+    if gripe_frame < 7:
+        gripe_frame += 1
+    else:
+        gripe_frame = 0
+    gripe.image = "gr" + str(int(gripe_frame))
 
 def gripe_move():
+    gripe.image = "gripe"
     if keyboard.left and gripe.left > 0:
+        gripe_walk_left()
         gripe.x -= gripe.speed
     elif keyboard.right and gripe.right < WIDTH:
+        gripe_walk_right()
         gripe.x += gripe.speed
 
 ## Green Apples
@@ -52,7 +73,7 @@ for i in range(number_of_green_apples):
 
 ## Red Apples
 red_apples = []
-number_of_red_apples = 3
+number_of_red_apples = 2
 
 def red_apples_spawn():
     red_apples.append(Actor("applered", pos = (randint(8, WIDTH -  8), 16)))
@@ -72,10 +93,16 @@ for i in range(number_of_red_apples):
 def draw():
     screen.blit(images.background, (0, 0))
     gripe.draw()
-    for apple in green_apples:
-        apple.draw()
-    for apple in red_apples:
-        apple.draw()
+    
+    if not game_over:
+        for apple in green_apples:
+            apple.draw()
+        for apple in red_apples:
+            apple.draw()
+    else:
+        gripe.image = "gdie"
+        screen.draw.text("Game Over", center = (CENTER, HEIGHT/2), color = "red", fontsize = 64)
+    
     for i in range(15):
         screen.blit(images.ground, (i*GROUNDWIDTH, HEIGHT - 32))
         screen.blit(images.checker, (i*GROUNDWIDTH, HEIGHT - 16))
@@ -84,16 +111,17 @@ def draw():
 
 def update():
     global score, game_over
-    gripe_move()
+    if not game_over:
+        gripe_move()
     
-    for apple in green_apples:
-        green_apple_fall(apple)
-        if gripe.colliderect(apple):
-            apple.pos = (randint(8, WIDTH - 8), 16)
-            score += 1
-    for apple in red_apples:
-        red_apple_fall(apple)
-        if gripe.colliderect(apple):
-            game_over = True
+        for apple in green_apples:
+            green_apple_fall(apple)
+            if gripe.colliderect(apple):
+                apple.pos = (randint(8, WIDTH - 8), 16)
+                score += 1
+        for apple in red_apples:
+            red_apple_fall(apple)
+            if gripe.colliderect(apple):
+                game_over = True
 
 pgzrun.go()
