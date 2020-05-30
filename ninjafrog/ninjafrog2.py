26be24d12d07c5c07d68e2d131fa1998a5005a28
Jörg Kantel <joerg@kantel.de>
2020-05-30 14:37:09 +0200

    ninjafrog2 finished

--- ninjafrog/ninjafrog2.py
+++ ninjafrog/ninjafrog2.py
@@ -19,6 +19,9 @@ ninja_x_velocity = 0
 ninja_y_velocity = 0
 gravity = 1
 jumping = False
+jumped = False
+allow_x = True
+timer = []
 
 platform_x = [20, 180, 360, 540, 100, 480, 20, 180, 360, 540, 280]
 platform_y = [120, 120, 120, 120, 220, 220, 320, 320, 320, 320, 400]
@@ -42,15 +45,59 @@ def update():
     ninja_move()
 
 def ninja_move():
-    global ninja_x_velocity, ninja_y_velocity, jumping, gravity
+    global ninja_x_velocity, ninja_y_velocity, jumping, jumped, gravity, allow_x, timer
+    # Facing the Front
+    if ninja_x_velocity == 0 and not jumped:
+        ninja.image = "ninja_idle1"
     # Gravity
     if collide_check():
         gravity = 1
         ninja.y -= 1
+        allow_x = True
+        timer = []
     if not collide_check():
         ninja.y += gravity
         if gravity <= 20:
             gravity += 0.5
+        timer.append(pygame.time.get_ticks())
+        if len(timer) > 5 and not jumped:
+            ninja.image = "ninja_jump"
+            if len(timer) > 20:
+                ninja.image = "ninja_fall"
+    # Left and Right Movement
+    if keyboard.left and allow_x:
+        if (ninja.x > 16) and (ninja_x_velocity > -8):
+            ninja_x_velocity -= 2
+            ninja.image = "run_l1"
+            if keyboard.left and jumped:
+                ninja.image = "ninja_jump_l"
+    if keyboard.right and allow_x:
+        if (ninja.x < WIDTH - 16) and (ninja_x_velocity < 8):
+            ninja_x_velocity += 2
+            ninja.image = "run_r1"
+            if keyboard.right and jumped:
+                ninja.image = "ninja_jump_r"
+    ninja.x += ninja_x_velocity
+    # Velocity
+    if ninja_x_velocity > 0:
+        ninja_x_velocity -= 1
+    if ninja_x_velocity < 0:
+        ninja_x_velocity += 1
+    if (ninja.x < 20) or (ninja.x > WIDTH - 20):
+        ninja_x_velocity = 0
+    # Jumping
+    if keyboard.up and collide_check() and not jumped:
+        jumping = True
+        jumped = True
+        clock.schedule_unique(jumped_recently, 0.4)
+        ninja.image = "ninja_jump"
+        ninja_y_velocity = 95
+    if jumping and ninja_y_velocity > 25:
+        ninja_y_velocity = ninja_y_velocity - ((100 - ninja_y_velocity)/2)
+        ninja.y -= ninja_y_velocity/3
+    else:
+        ninja_y_velocity = 0
+        jumping = False
 
 def collide_check():
     collide = False
@@ -58,6 +105,10 @@ def collide_check():
         if ninja.colliderect(platrect):
             collide = True
     return(collide)
+
+def jumped_recently():
+    global jumped
+    jumped = False
 
 def background_colour_fade():
     global blue, blueforward
