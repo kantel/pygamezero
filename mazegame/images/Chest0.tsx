8b3d258e8f2aaee2f70fd471af7ee480acdec45b
Jörg Kantel <joerg@kantel.de>
2022-04-15 19:49:20 +0200

    Mazegame Stage 1 cont.

---
+++ mazegame/images/Chest0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Chest0" tilewidth="16" tileheight="16" tilecount="24" columns="8">
+ <image source="../Dropbox/pd_gamegraphics/DawnLike_4/Items/Chest0.png" width="128" height="48"/>
+</tileset>
---
+++ mazegame/images/Wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Wall" tilewidth="16" tileheight="16" tilecount="1020" columns="20">
+ <image source="../Dropbox/pd_gamegraphics/DawnLike_4/Objects/Wall.png" width="320" height="816"/>
+</tileset>
