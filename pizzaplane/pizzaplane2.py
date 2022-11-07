42910f2b2a9b52ff7e0fc4fb7c129e46168f5a45
Jörg Kantel <joerg@kantel.de>
2022-11-07 17:17:50 +0100

    Update pizzaplane2.py

--- pizzaplane/pizzaplane2.py
+++ pizzaplane/pizzaplane2.py
@@ -10,31 +10,54 @@ from random import randint
 WIDTH = 720
 HEIGHT = 480
 TITLE = "Pizza Plane Stage 2 (mit Missile und pösen Pizzas)"
-LEFT = WIDTH/2
-BOTTOM = HEIGHT/2
-NO_ENEMIES = 10
 
 # Konstanten
+BG_LEFT = WIDTH/2
+BG_BOTTOM = HEIGHT/2
 BG_WIDTH = 1067             # Breite des Hintergrundbildes
-BG_WIDTH2   = 533           # BGWIDTH//2 (abgerundet)
+BG_WIDTH2   = 533           # BG_WIDTH//2 (abgerundet)
 BACKGROUND = "desert"
+NO_ENEMIES = 10
 PLANE_X = 70                # X-Position des Fliegers (bleibt fest)
 
 # Klassendefinitionen
 
+class Plane(Actor):
+    
+    def __init__(self):
+        Actor.__init__(self, "planered_0", (PLANE_X, 200))
+        self.images = []
+        for i in range(3):
+            img = "planered_" + str(i)
+            self.images.append(img)
+        self.r = 0
+        self.updown = 3
+        self.dir = "NONE"
+        
+    def update(self):
+        if self.dir == "NONE":
+            self.y += 0
+        elif self.dir == "UP":
+            if self.y > 20:
+                self.y -= self.updown
+        elif self.dir == "DOWN":
+            if self.y < HEIGHT - 20:
+                self.y += self.updown
+        self.image = self.images[int(self.r)]
+        self.r += 0.2
+        if self.r >= 3.0:
+            self.r = 0
+
 class Missile(Actor):
     
     def __init__(self):
-        Actor.__init__(self, "laserred")
-        self.x = plane.x
-        self.y = plane.y
+        Actor.__init__(self, "laserred", (plane.x, plane.y))
         self.speed = 25
         self.fire = False
     
     def update(self):
         if not self.fire:
-            self.x = plane.x
-            self.y = plane.y
+            self.pos = plane.x, plane.y
         else:
             self.x += self.speed
             if self.x >= WIDTH:
@@ -51,8 +74,12 @@ class Pizza(Actor):
     def reset(self):
         self.x = randint(WIDTH + 50, WIDTH + 750)
         self.y = randint(self.height, HEIGHT - self.height)
+        self.speed = -randint(2, 5)
     
     def update(self):
+        if self.colliderect(missile) and missile.fire:
+            self.reset()
+            missile.fire = False
         self.x += self.speed
         if self.x <= -self.width:
             self.reset()        
@@ -60,18 +87,11 @@ class Pizza(Actor):
 # Init Game
 # Hintergrund
 backs = []
-back0 = Actor(BACKGROUND, (LEFT, BOTTOM))
-back1 = Actor(BACKGROUND, (BG_WIDTH + LEFT, BOTTOM))
+back0 = Actor(BACKGROUND, (BG_LEFT, BG_BOTTOM))
+back1 = Actor(BACKGROUND, (BG_WIDTH + BG_LEFT, BG_BOTTOM))
 backs = [back0, back1]
 # Player (Flieger)
-plane_image = []
-for i in range(3):
-    img = "planered_" + str(i)
-    plane_image.append(img)
-plane = Actor(plane_image[0], (PLANE_X, 200))   # Startposition
-plane.r = 0
-plane.updown = 3
-plane.dir = "NONE"
+plane = Plane()
 # Missile
 missile = Missile()
 pizzas = []
@@ -89,23 +109,9 @@ def update():
     missile.update()
     ## Pizzas
     for pizza in pizzas:
-        if pizza.colliderect(missile) and missile.fire:
-            pizza.reset()
-            missile.fire = False
         pizza.update()
     ## Flieger
-    if plane.dir == "NONE":
-        plane.y += 0
-    elif plane.dir == "UP":
-        if plane.y > 20:
-            plane.y -= plane.updown
-    elif plane.dir == "DOWN":
-        if plane.y < HEIGHT - 20:
-            plane.y += plane.updown
-    plane.image = plane_image[int(plane.r)]
-    plane.r += 0.2
-    if plane.r >= 3.0:
-        plane.r = 0
+    plane.update()
             
 def draw():
     screen.fill("#94b0c2")  # Light gray
@@ -124,7 +130,7 @@ def on_key_down():
         plane.dir = "DOWN"
     ## Feuern mit rechter Pfeiltaste
     if keyboard.right:
-        print("FIRE!")
+        # print("FIRE!")
         missile.fire = True
     ## Spielende mit ESC
     if keyboard.escape:
